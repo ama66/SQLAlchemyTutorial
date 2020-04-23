e3f117925c79dc0abe41357faeec93c18f2cf88c
Ammar Abdilghanie <ama66@cornell.edu>
2020-04-23 14:48:51 -0500

    Update sql-alechmy.py

--- sql-alechmy.py
+++ sql-alechmy.py
@@ -874,3 +874,113 @@ results = connection.execute(stmt, values_list)
 # Print rowcount
 print(results.rowcount)
 
+
+
+# Import select and func
+from sqlalchemy import select, func
+
+# Select the average of age weighted by pop2000
+stmt = select([func.sum(census.columns.pop2000 * census.columns.age) 
+ / func.sum(census.columns.pop2000)
+ ])
+
+# Import select and func
+from sqlalchemy import select, func
+
+# Relabel the new column as average_age
+stmt = select([(func.sum(census.columns.pop2000 * census.columns.age) 
+/ func.sum(census.columns.pop2000)).label('average_age')
+ ])
+
+# Import select and func
+from sqlalchemy import select, func
+
+# Add the sex column to the select statement
+stmt = select([census.columns.sex,
+    (func.sum(census.columns.pop2000 * census.columns.age) 
+ / func.sum(census.columns.pop2000)).label('average_age')               
+ ])
+
+# Group by sex
+stmt = stmt.group_by(census.columns.sex)
+
+
+# Import select and func
+from sqlalchemy import select, func
+
+# Select sex and average age weighted by 2000 population
+stmt = select([(func.sum(census.columns.pop2000 * census.columns.age) 
+/ func.sum(census.columns.pop2000)).label('average_age'),
+               census.columns.sex
+])
+
+# Group by sex
+stmt = stmt.group_by(census.columns.sex)
+
+# Execute the query and fetch all the results
+results = connection.execute(stmt).fetchall()
+
+# Print the sex and average age column for each result
+for result in results:
+    print(result.sex, result.average_age)
+    
+    
+    
+# import case, cast and Float from sqlalchemy
+from sqlalchemy import case, cast, Float
+
+# Build a query to calculate the percentage of women in 2000: stmt
+stmt = select([census.columns.state,
+    (func.sum(
+        case([
+            (census.columns.sex == 'F', census.columns.pop2000)
+        ], else_=0)) /
+     cast(func.sum(census.columns.pop2000), Float) * 100).label('percent_female')
+])
+
+# Group By state
+stmt = stmt.group_by(census.columns.state)
+
+# Execute the query and store the results: results
+results = connection.execute(stmt).fetchall()
+
+# Print the percentage
+for result in results:
+    print(result.state, result.percent_female)
+
+    
+# Build query to return state name and population difference from 2008 to 2000
+stmt = select([census.columns.state,
+     (census.columns.pop2008-census.columns.pop2000).label('pop_change')
+])
+
+# Group by State
+stmt = stmt.group_by(census.columns.state)
+
+# Order by Population Change
+stmt = stmt.order_by(desc('pop_change'))
+
+# Limit to top 10
+stmt = stmt.limit(10)
+
+# Use connection to execute the statement and fetch all results
+results = connection.execute(stmt).fetchall()
+
+# Print the state and population change for each record
+for result in results:
+    print('{}:{}'.format(result.state, result.pop_change))
+    
+    
+# California:105705
+#     Florida:100984
+#     Texas:51901
+#     New York:47098
+#     Pennsylvania:42387
+#     Arizona:29509
+#     Ohio:29392
+#     Illinois:26221
+#     Michigan:25126
+#     North Carolina:24108
+        
+        
+    
